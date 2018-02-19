515d2b333c6041afddabf5817755c26a0ac7c18b
Hongzhi Liu <liu.hongz@husky.neu.edu>
2018-02-19 00:40:27 -0500

    Refactor some functions

--- training_SVM.m
+++ training_SVM.m
@@ -1,14 +1,14 @@
 
 load('train.mat');
 
-fprintf('\nTraining... \n')
+fprintf('\nTraining SVM... \n')
 C = 10;
 model = svmTrain(train{10},label{10}, C, @gaussianKernel);
 
 load('test.mat');
 p = svmclassify(model, test);
 
-fprintf('Test Accuracy: %f\n', mean(double(p == tlabel)) * 100);
+fprintf('Cross Validation Accuracy: %f\n', mean(double(p == tlabel)) * 100);
 
 [weight, idx] = sort(model.w, 'descend');
 vocabList = getVocabList();
@@ -17,5 +17,3 @@ fprintf('\nTop 10 predictors of spam: \n');
 for i = 1:10
     fprintf(' %-10s (%f) \n', vocabList{idx(i)}, weight(i));
 end
-
-fprintf('\nPress enter to continue.\n');
--- training_naivebayes.m
+++ training_naivebayes.m
@@ -1,7 +1,6 @@
 load('train.mat');
-fprintf('\nTraining... \n')
+fprintf('Training Naive Bayes... \n')
 [p0Vec, p1Vec, pSpam] = trainNaiveBayes(train{10}, label{10});
 load('test.mat');
 label = classifyNaiveBayes(test, p0Vec, p1Vec, pSpam);
-fprintf('Test Accuracy: %f\n', mean(double(label == tlabel)) * 100);
-fprintf('\nPress enter to continue.\n');
+fprintf('Cross Validation Accuracy: %f\n', mean(double(label == tlabel)) * 100);
